8f7e02d95c731715a9da71ffb8d77e68e197cf8e
Muhammad Irfan <101461077+irfanmustafvi@users.noreply.github.com>
2024-10-07 23:45:15 +0300

    Delete PythonLiveWorkshop directory

--- PythonLiveWorkshop/5.utils.py
+++
@@ -1,5 +0,0 @@
-import os  # importing a new library in code
-
-print(os.system("systeminfo"))  # windows, linux or mac
-# print(os.system("uptime"))  # for linux uptime and load average
-print(os.system("wmic computersystem get totalphysicalmemory"))  # RAM in window
